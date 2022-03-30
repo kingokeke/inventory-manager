b3970ebd900f7338050b2021334c54518fa84087
Kingsley Okeke <mr.kingsley.okeke@gmail.com>
2022-03-30 05:23:16 +0100

    Added sidebar link component

---
+++ src/components/SidebarLink.vue
@@ -0,0 +1,19 @@
+<template>
+  <router-link :to="url" class="nav-link p-5 sidebar-link">{{
+    title
+  }}</router-link>
+</template>
+
+<script setup lang="ts">
+  interface SidebarLinkProps {
+    title: string;
+    url: string;
+  }
+  defineProps<SidebarLinkProps>();
+</script>
+
+<style>
+  .sidebar-link:hover {
+    background-color: #ddd;
+  }
+</style>
