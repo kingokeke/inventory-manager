1bfec348a1595017799fbbf1d8a825087f58c6df
Kingsley Okeke <mr.kingsley.okeke@gmail.com>
2022-03-30 05:20:22 +0100

    Added routes for sidebar links

---
+++ src/data/navLinks.ts
@@ -0,0 +1,24 @@
+const navLinks = [
+  {
+    title: "Dashboard",
+    url: "/dashboard",
+  },
+  {
+    title: "Sales",
+    url: "/sales",
+  },
+  {
+    title: "Inventory",
+    url: "/inventory",
+  },
+  {
+    title: "Reports",
+    url: "/reports",
+  },
+  {
+    title: "Settings",
+    url: "/settings",
+  }
+];
+
+export default navLinks;
