9908fd30b17bda925b4f49911aabe5bea6f80d0c
Kingsley Okeke <mr.kingsley.okeke@gmail.com>
2022-04-01 13:49:19 +0000

    Added table headers for order list

---
+++ src/data/tableHeaders.ts
@@ -0,0 +1,9 @@
+export const orderListTableHeaders = [
+  { title: "S/N", class: 'text-center' },
+  { title: "Product", class: 'w-40' },
+  { title: "Price", class: 'text-center' },
+  { title: "Qty", class: 'text-center' },
+  { title: "Disc", class: 'text-center' },
+  { title: "Total", class: 'text-center' },
+  { title: "", class: 'text-center' },
+]
