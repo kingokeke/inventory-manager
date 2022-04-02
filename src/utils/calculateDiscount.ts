2f26f9e3d13cac454fb0b4d7bd44faafce5adce3
Kingsley Okeke <mr.kingsley.okeke@gmail.com>
2022-04-02 15:49:19 +0000

    Added util function to calculate total

--- src/utils/calculateDiscount.ts
+++ src/utils/calculateDiscount.ts
@@ -3,3 +3,7 @@ import { IProduct } from "@/models/interfaces";
 export const calculateDiscount = (product: IProduct, quantity: number) => (
   quantity >= product.discountQty ? (product.price - product.discountPrice) * quantity : 0
 );
+
+export const calculateTotal = (product: IProduct, quantity: number) => (
+  product.price * quantity - calculateDiscount(product, quantity)
+);
